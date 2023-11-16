6e9eea5f368348173914300e8167e9ca3fbbae92
Amasha17 <140387741+Amasha17@users.noreply.github.com>
2023-11-16 11:36:26 +0530

    Create institutionforms.php

---
+++ Forms/InstitutionForm/institutionforms.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$cnumber = $_POST['contnumber'];
+$uname = $_POST['username'];
+$password = $_POST['passwo'];
+$pretype = $_POST['pass'];
+$district = $_POST['District'];
+$insweb = $_POST['inswebsite'];
+
+$username = "root";
+$password = "";
+$database = "forms";
+$table = "instituions";
+
+?>
