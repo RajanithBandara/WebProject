c42f968e33f2d94585e2f4c9521111203f4a1470
Dinuki <135609702+Dinuki19@users.noreply.github.com>
2023-11-16 16:08:20 +0530

    Update stdform.php

--- Forms/StudentForm/stdform.php
+++ Forms/StudentForm/stdform.php
@@ -3,18 +3,36 @@ $name = $_POST['name'];
 $email=$_POST['email'];
 $photo = $_POST['photo'];
 $age = $_POST['age'];
-$dis = $_POST['district'];
+$district = $_POST['district'];
 $number = $_POST['number'];
 $grade = $_POST['grade'];
 $medium = $_POST['medium'];
-$uname = $_POST['txtnme'];
-$password = $_POST['pwd1'];
-$pretype = $_POST['pwd2'];
+$txtnme = $_POST['txtnme'];
+$pwd1 = $_POST['pwd1'];
+$pwd2= $_POST['pwd2'];
 $gname = $_POST['gname'];
-$cnum = $_POST['cnumber'];
+$cnumber = $_POST['cnumber'];
+
 $username = "root";
-$password = " ";
+$password = "";
 $database = "forms";
 $table = "student";
 $table2 = "guardian";
+$server="Localhost";
+$conn= new mysqli($server,$username,$password,$database);
+ if ($conn->connect_error)
+ {
+    die("Connection failed:".$conn->connect_error);
+ }
+ else
+{
+    echo "success";
+    $s="Insert into guardian values('$email','$gname','$cnumber')";
+    mysqli_query($conn,$s);
+    if ($pwd1==$pwd2){
+        $s1="Insert into student values('$email','$name','$photo','$age','$district','$number','$grade','$medium','$txtnme','$pwd1')";
+        echo "Your data entered successfully";
+        mysqli_query($conn,$s1);
+    }
+}
 ?>
