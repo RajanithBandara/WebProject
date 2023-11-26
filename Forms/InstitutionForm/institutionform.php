693534695157335c35ce74b164ca32ddc7051609
Amasha17 <140387741+Amasha17@users.noreply.github.com>
2023-11-26 18:14:58 +0530

    php for institute form

--- Forms/InstitutionForm/institutionform.php
+++ Forms/InstitutionForm/institutionform.php
@@ -4,14 +4,32 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $cnumber = $_POST['contnumber'];
 $uname = $_POST['username'];
-$password = $_POST['passwo'];
+$inspassword = $_POST['passwo'];
 $pretype = $_POST['pass'];
 $district = $_POST['District'];
 $insweb = $_POST['inswebsite'];
+
 
 $username = "root";
 $password = "";
 $database = "forms";
 $table = "instituions";
+$server="Localhost";
+
+$conn=new mysqli($server,$username,$password,$database);
+if($conn->connect_error)
+{
+    die("Connection failed:".$conn->connect_error);
+}
+else
+{
+    echo"success <br>";
+    if($inspassword==$pretype)
+    {
+        $s="Insert into instituions values('$name','$email','$cnumber','$uname','$inspassword','$district','$insweb')";
+        mysqli_query($conn,$s);
+        echo"Your data entered successfully";
+    }
+}
 
 ?>
